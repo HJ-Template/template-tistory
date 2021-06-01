24ad689014ec91fdd3920786cbe4a93db88db40c
as-you-say <dgf5820@naver.com>
2021-06-02 08:28:10 +0900

    Add tistory skin default files

---
+++ images/category.js
@@ -0,0 +1,29 @@
+const category = [
+  {
+    id: 1,
+    name: "category 1",
+    link: "",
+    subCategory: [
+      { id: 10, name: "category 1-1", link: "" },
+      { id: 11, name: "category 1-2", link: "" },
+    ],
+  },
+  {
+    id: 2,
+    name: "category 2",
+    link: "",
+    subCategory: [
+      { id: 20, name: "category 2-1", link: "" },
+      { id: 21, name: "category 2-2", link: "" },
+    ],
+  },
+  {
+    id: 3,
+    name: "category 3",
+    link: "",
+    subCategory: [
+      { id: 30, name: "category 3-1", link: "" },
+      { id: 31, name: "category 3-2", link: "" },
+    ],
+  },
+];
